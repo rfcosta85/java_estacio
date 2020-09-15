ba12a81e766b6ff5392e39517bc07dd24f87def1
Rodrigo Costa <57047212+rfcosta85@users.noreply.github.com>
2020-09-15 10:18:38 -0300

    aula06

---
+++ aula_06/Automovel.java
@@ -0,0 +1,26 @@
+package aula_06;
+
+public class Automovel 
+{
+
+    String modelo, cor;
+    int ano;
+    boolean estadoMotor;
+
+
+
+private void ligarMotor(boolean estadoMotor) 
+{
+    
+    estadoMotor = true;
+
+}
+
+private void desligarMotor(boolean estadoMotor)
+{
+
+    estadoMotor = false;
+
+}
+
+}
